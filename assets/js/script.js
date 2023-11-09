7956bbb7f04181c50d28362766dbc0abbf514371
User <user@Users-MacBook-Air.local>
2023-11-08 21:04:13 -0500

    edit script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-let API_KEY = "49322d6a831b4624912f7804ed195b0a"
+let API_KEY = "49322d6a831b4624912f7804ed195b0a";
+console.log('works')
+
+// fetch(
+//   `https://api.spoonacular.com/recipes/716429/information?apiKey=${API_KEY}`
+// ).then(data => {
+//     console.log(data)
+// })
